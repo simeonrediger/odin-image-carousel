0fe8407215e60fbece3d740ddad2205d0c5f48bb
Simeon Rediger <simeonrediger@gmail.com>
2025-11-29 14:23:11 -0500

    Remove shadowing instances declaration in init()

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const attribute = 'data-image-carousel';
 let instances;
 
 function init(root = document) {
-    let instances = Array.from(root.querySelectorAll(`[${attribute}]`));
+    instances = Array.from(root.querySelectorAll(`[${attribute}]`));
 }
 
 const imageCarousel = {
