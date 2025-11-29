d9fa4242589c5f4edcb27e3f474d4678439df197
Simeon Rediger <simeonrediger@gmail.com>
2025-11-29 14:47:36 -0500

    Warn if any children are not img elements

--- src/image-carousel.js
+++ src/image-carousel.js
@@ -3,5 +3,21 @@ export default class ImageCarousel {
 
     constructor(containerElement) {
         this.#container = containerElement;
+        this.#build();
+    }
+
+    #build() {
+        const images = Array.from(this.#container.children);
+        this.#validateImages(images);
+    }
+
+    #validateImages(images) {
+        if (images.some(image => image.tagName !== 'IMG')) {
+            console.warn(
+                'Image carousel contains children that are not <img> elements.',
+                'This may result in abnormal behavior.',
+                this.#container,
+            );
+        }
     }
 }
