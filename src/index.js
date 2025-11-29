3b7ff8eda0e484070e8075d268f1d1e0ac975e0b
Simeon Rediger <simeonrediger@gmail.com>
2025-11-29 14:36:28 -0500

    Construct each image carousel with ImageCarousel()

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
+import ImageCarousel from './image-carousel.js';
+
 const imageCarouselAttribute = 'data-image-carousel';
 
 function init(root = document) {
     const imageCarousels = root.querySelectorAll(`[${imageCarouselAttribute}]`);
+
+    for (const imageCarousel of imageCarousels) {
+        new ImageCarousel(imageCarousel);
+    }
 }
 
 const imageCarousel = {
